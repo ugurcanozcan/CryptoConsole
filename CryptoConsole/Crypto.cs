8488c9f4c6b275128c1aa05e479128f92c215d6f
ugurcanozcan <ugurcanozcan3@gmail.com>
2022-07-16 19:42:51 +0300

    Add project files.

---
+++ CryptoConsole/Crypto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoConsole
+{
+    internal static class Crypto
+    {
+        public static string Sifrele(string metin)
+        {
+            byte[] metinByte = UTF8Encoding.UTF8.GetBytes(metin);
+
+            // Sistemin kripto servisine ait classı intsence alındı.
+            using (MD5CryptoServiceProvider provider = new MD5CryptoServiceProvider())
+            {
+                //anahtarımız şifrelemeye hazırlanmak üzere byte çevrildi.
+                byte[] anahtarlar = provider.ComputeHash(UTF8Encoding.UTF8.GetBytes("Galatasaray"));
+
+                using (TripleDESCryptoServiceProvider serviceProvider = new TripleDESCryptoServiceProvider()
+                { Key = anahtarlar, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+
+                    ICryptoTransform transform = serviceProvider.CreateEncryptor();
+                    byte[] sonuc = transform.TransformFinalBlock(metinByte, 0, metinByte.Length);
+                    return Convert.ToBase64String(sonuc);
+                }
+            }
+
+
+        }
+
+        public static string SifreCoz(string sifreliMetin)
+        {
+            byte[] sifreliMetinByte = Convert.FromBase64String(sifreliMetin);
+            using (MD5CryptoServiceProvider provider = new MD5CryptoServiceProvider())
+            {
+                //anahtarımız şifrelemeye hazırlanmak üzere byte çevrildi.
+                byte[] anahtarlar = provider.ComputeHash(UTF8Encoding.UTF8.GetBytes("Galatasaray"));
+
+                using (TripleDESCryptoServiceProvider serviceProvider = new TripleDESCryptoServiceProvider()
+                { Key = anahtarlar, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+
+                    ICryptoTransform transform = serviceProvider.CreateDecryptor();
+                    byte[] sonuc = transform.TransformFinalBlock(sifreliMetinByte, 0, sifreliMetinByte.Length);
+
+                    return UTF8Encoding.UTF8.GetString(sonuc);
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ CryptoConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace CryptoConsole
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Şifrelenecek metini giriniz ve enter tuşuna basınız:");
+
+            string metin = Console.ReadLine();
+
+            Console.WriteLine(Crypto.Sifrele(metin));
+          
+
+
+            Console.WriteLine("Çözmek istediğiniz şifreli metni giriniz: ");
+            string sifreliMetin = Console.ReadLine();
+            Console.WriteLine(Crypto.SifreCoz(sifreliMetin));
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
